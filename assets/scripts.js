49ecc2e752b37ce34c6c22524e675ae03334362a
screensmartducklings <147714673+screensmartducklings@users.noreply.github.com>
2023-11-25 12:42:56 +0300

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,15 +4,19 @@ $(function () {
 
 $('#search-btn').click(function() {
   var searchTerm = $('#search-input').val().toLowerCase();
+  var regex = new RegExp(`(${searchTerm})`, 'gi'); // Regular expression for case-insensitive search
+
   $('.post-preview').hide(); // Hide all posts initially
 
   $('.post-preview').each(function() {
-      var postTitle = $(this).find('.post-title').text().toLowerCase();
-      var postSubtitle = $(this).find('.post-subtitle').text().toLowerCase();
-      var postContent = $(this).text().toLowerCase(); // This includes title, subtitle, and other content
+    var postContent = $(this).text().toLowerCase(); // This includes title, subtitle, and other content
 
-      if (postTitle.includes(searchTerm) || postSubtitle.includes(searchTerm) || postContent.includes(searchTerm)) {
-          $(this).show(); // Show posts that match the search term in title, subtitle, or content
-      }
+    if (postContent.includes(searchTerm)) {
+      $(this).show(); // Show posts that match the search term
+
+      // Highlight the searched term in the content
+      var highlightedContent = $(this).html().replace(regex, '<span class="highlight">$1</span>');
+      $(this).html(highlightedContent);
+    }
   });
 });
