a30cb5a388065dfe60ccd83d46e5b1d5978ef0ac
screensmartducklings <147714673+screensmartducklings@users.noreply.github.com>
2023-12-04 15:12:35 +0300

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -43,3 +43,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+document.addEventListener('DOMContentLoaded', function() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, {
+    threshold: 0.1 // Adjust as needed
+  });
+
+  const posts = document.querySelectorAll('.post-item');
+  posts.forEach(post => observer.observe(post));
+});
+
